dc3782f6a087cb171272bd0dfe089b8048b6f3fd
ionush <neilasjoseph@gmail.com>
2018-12-27 20:11:15 +0800

    clock

---
+++ main.js
@@ -0,0 +1,24 @@
+const clock = () => {
+    const fullDate = new Date()
+    let hours = fullDate.getHours()
+    let mins = fullDate.getMinutes()
+    let secs = fullDate.getSeconds()
+
+    if (hours < 10) {
+        hours = "0" + hours
+    }
+
+    if (mins < 10) {
+        mins = "0" + mins
+    }
+
+    if (secs < 10) {
+        secs = "0" + secs
+    }
+    document.getElementById('hour').innerHTML = hours + ":"
+    document.getElementById('min').innerHTML = mins + ":"
+    document.getElementById('sec').innerHTML = secs
+
+}
+
+setInterval(clock, 1000)
