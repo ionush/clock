6d831858c5158d40724efef8000d0e29d5466b2d
ionush <neilasjoseph@gmail.com>
2019-01-04 02:31:19 +0800

    code cleanup

--- main.js
+++ main.js
@@ -11,32 +11,23 @@ const { children } = document.getElementsByClassName('main')[0]
 
 const templatingEngine = (store, children) => {
   const replace = childBranch => {
-    const keys = Object.keys(store)
     const id = childBranch.id
-    const found = keys.find(e => e === id)
-    if (found && store[id] !== childBranch.innerHTML) {
-      childBranch.innerHTML = store[id]
-    }
+    const found = Object.keys(store).find(e => e === id)
+    if (found && store[id] !== childBranch.innerHTML) childBranch.innerHTML = store[id]
   }
   for (let i = 0; i < children.length; i++) {
-    const length = children[i].children.length
-    if (length === 0) {
-      replace(children[i])
-    } else if (length > 0) {
+    const { length } = children[i].children
+    if (length === 0) replace(children[i])
+    else if (length > 0) {
       for (let j = 0; j < length; j++) {
         const nestedLength = children[i].children[j].children.length
         if (nestedLength > 0) {
           const nestedChildren = children[i].children[j].children
           templatingEngine(store, nestedChildren)
-        } else {
-          replace(children[i].children[j])
-        }
+        } else replace(children[i].children[j])
       }
     }
   }
 }
 setInterval(clock, 1000)
-setInterval(() => {
-  templatingEngine(time, children)
-}, 1000)
-// templatingEngine(time, children)
+setInterval(() => templatingEngine(time, children), 1000)
